8ff21c597379262915110b144c1c73d4a10b83a7
Mbokola <tonypeaks@gmail.com>
2023-11-29 09:17:52 +0000

    Task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -29,6 +29,6 @@ bob@dylan:~$ npm run dev 3-main.js
 41
 bob@dylan:~$
 */
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
