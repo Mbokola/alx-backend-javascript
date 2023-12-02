27798d4a98d8f0e29144fdcf5eb724e218695ba1
Mbokola <tonypeaks@gmail.com>
2023-12-02 18:26:35 +0000

    Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,17 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-      .then((results) => {
-        results.map((result) => (
+    .then((results) => {
+      const allresults = [];
+
+      for (const result of results) {
+        allresults.push(
           {
             status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-          }));
-      });
+            value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+          },
+        );
+      }
+      return allresults;
+    });
 }
