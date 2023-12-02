a1ef4dd5f230046f75f2af4f14c14bb1fff41125
Mbokola <tonypeaks@gmail.com>
2023-12-02 12:17:10 +0000

    Return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([uploadResult, user]) => {
       const value = uploadResult.body;
       const name = `${user.firstName} ${user.lastName}`;
       console.log(`${value} ${name}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
