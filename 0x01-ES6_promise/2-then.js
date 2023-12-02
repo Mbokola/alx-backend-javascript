854bc005e63f0f166c4c046d620732ca81dca46d
Mbokola <tonypeaks@gmail.com>
2023-12-02 10:49:56 +0000

    Output error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((resolved) => resolved)
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
     .catch((rejectError) => rejectError)
     .finally(() => {
       console.log('Got a response from the API');
