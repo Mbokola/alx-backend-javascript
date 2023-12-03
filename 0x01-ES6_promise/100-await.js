7f2c25de5f0608da55dc5bc61ea81680edd3d45d
Mbokola <tonypeaks@gmail.com>
2023-12-03 06:41:19 +0000

    async function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+const asyncUploader = async () => {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+};
+
+export default asyncUploader;
