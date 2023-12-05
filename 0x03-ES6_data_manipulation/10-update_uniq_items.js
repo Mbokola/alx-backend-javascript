13b7d9325a714f48fcfb183bbe63de02dbbaa5dd
Mbokola <tonypeaks@gmail.com>
2023-12-05 19:44:05 +0000

    Final

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+export default function updateUniqueItems(inputMap) {
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  const updatedMap = new Map();
+
+  for (const [key, value] of inputMap) {
+    if (value === 1) {
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+
+  return updatedMap;
+}
