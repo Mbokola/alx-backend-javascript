1bcd4aa9d2588fe3e7b171842a678772424b1b84
Mbokola <tonypeaks@gmail.com>
2024-01-30 17:38:44 +0000

    Refactored

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,9 +4,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   if (process.send) {
-    console.log()
+    console.log();
   }
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
   process.exit();
 });
