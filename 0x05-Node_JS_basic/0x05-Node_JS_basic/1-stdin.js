4f17c499f094c7dadb4c41aee7c49711ce3946a7
Mbokola <tonypeaks@gmail.com>
2024-01-30 10:43:52 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
 // Read-in user input
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
-
-readline.question('Welcome to Holberton School, what is your name?', (name) => {
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  const name = data.trim();
   console.log();
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
+  process.exit();
 });
