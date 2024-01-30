3417d0918169c7cb2e48f966cb91d11ff43a904c
Mbokola <tonypeaks@gmail.com>
2024-01-30 12:42:32 +0000

    Try different

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,14 @@
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: false,
 });
 
-readline.question('Welcome to Holberton School, what is your name?', (name) => {
-  console.log();
+console.log('Welcome to Holberton School, what is your name?');
+
+readline.on('line', (name) => {
   console.log(`Your name is: ${name}`);
+});
+
+readline.on('close', () => {
   console.log('This important software is now closing');
 });
