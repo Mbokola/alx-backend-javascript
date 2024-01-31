582e758e3978e838f033de3f047c306ea321a522
Mbokola <tonypeaks@gmail.com>
2024-01-31 08:54:12 +0000

    export and throw error

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,44 +1,44 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  const data = fs.readFileSync(path, 'utf8');
-  if (!data) {
-    throw new Error('Cannot load the database');
-  }
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n');
+    const studentCount = lines.length - 1;
+    const allFields = lines.map((line) => {
+      const fields = line.split(',');
+      return {
+        firstField: fields[0],
+        lastField: fields[fields.length - 1],
+      };
+    });
 
-  const lines = data.split('\n');
-  const studentCount = lines.length - 1;
-  const allFields = lines.map((line) => {
-    const fields = line.split(',');
-    return {
-      firstField: fields[0],
-      lastField: fields[fields.length - 1],
-    };
-  });
+    const arr = [];
+    let names = '';
 
-  const arr = [];
-  let names = '';
-
-  for (let i = 1; i < allFields.length; i += 1) {
-    if (!arr.includes(allFields[i].lastField)) {
-      arr.push(allFields[i].lastField);
-    }
-  }
-
-  console.log(`Number of students: ${studentCount}`);
-
-  for (let i = 0; i < arr.length; i += 1) {
-    for (let j = 1; j < allFields.length; j += 1) {
-      if (allFields[j].lastField === arr[i]) {
-        names += `${allFields[j].firstField}, `;
+    for (let i = 1; i < allFields.length; i += 1) {
+      if (!arr.includes(allFields[i].lastField)) {
+        arr.push(allFields[i].lastField);
       }
     }
-    if (names.endsWith(', ')) {
-      names = names.slice(0, -2);
+
+    console.log(`Number of students: ${studentCount}`);
+
+    for (let i = 0; i < arr.length; i += 1) {
+      for (let j = 1; j < allFields.length; j += 1) {
+        if (allFields[j].lastField === arr[i]) {
+          names += `${allFields[j].firstField}, `;
+        }
+      }
+      if (names.endsWith(', ')) {
+        names = names.slice(0, -2);
+      }
+      console.log(`Number of students in ${arr[i]}: ${names.split(', ').length}. List: ${names}`);
+      names = '';
     }
-    console.log(`Number of students in ${arr[i]}: ${names.split(', ').length}. List: ${names}`);
-    names = '';
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 }
 
-countStudents('database.csv');
+module.exports = countStudents;
