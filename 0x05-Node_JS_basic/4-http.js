25d764eab3cd9d01b0db1aebd250152656870bfa
Mbokola <tonypeaks@gmail.com>
2024-01-31 18:05:31 +0000

    Set content-type header

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 });
