802ae59311582360035981021e9e5bfbd200a07c
Mbokola <tonypeaks@gmail.com>
2024-01-31 18:08:53 +0000

    Status code

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
   res.write('Hello Holberton School!');
   res.end();
 });
