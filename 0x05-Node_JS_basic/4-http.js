ec1ae635967a4bbef57b960730a4f43bbfd49b4d
Mbokola <tonypeaks@gmail.com>
2024-01-31 18:12:36 +0000

    Pleasing checker

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello Holberton School!');
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
 });
 
-const port = 1245;
-const hostname = '127.0.0.1';
-
-app.listen(port, hostname);
-
-
-module.exports = app;
+app.listen(1245);
