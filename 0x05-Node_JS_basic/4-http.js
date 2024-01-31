c6a9b7144c9279910666943569bafa139d4065f6
Mbokola <tonypeaks@gmail.com>
2024-01-31 18:15:04 +0000

    Revert

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.statusCode = 200;
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(1245);
+
+module.exports = app;
